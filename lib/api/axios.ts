22581b245ba1041822a37e95a4f0ca07a71f90bc
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-09 13:41:15 -0500

    feat: agregar interceptor JWT en Axios

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080/api/v1",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ??
+    "http://localhost:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
